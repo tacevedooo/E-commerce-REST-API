ae2bf074aa289a08c4a9012529c9a38959f9b9fe
tacevedooo <tacevedo@unal.edu.co>
2026-01-30 13:36:57 -0500

    init

---
+++ ecommercerestapi/src/main/java/com/ecommerce/restapi/EcommercerestapiApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercerestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercerestapiApplication.class, args);
+	}
+
+}
---
+++ ecommercerestapi/src/test/java/com/ecommerce/restapi/EcommercerestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercerestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
